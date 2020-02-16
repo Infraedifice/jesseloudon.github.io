dcf855159e57ec8166811bb57d785f4fb9cfd86d
jesseloudon <13147870+jesseloudon@users.noreply.github.com>
2020-02-17 10:53:36 +1100

    a

---
+++ _pages/Blogging Toolkit.markdown
@@ -0,0 +1,59 @@
+---
+title:  "Blogging Toolkit"
+permalink: /Blogging Toolkit/
+header:
+  image: "/assets/images/Keep-Going.jpg"
+  caption: ""
+excerpt: "A collection of links to help you get setup with Open Source blogging."
+date:   "2020-02-16"
+toc: "true"
+categories: 
+- "How-To"
+tags: 
+- "Blogging"
+- "Open Source"
+---
+A collection of links to help you get setup with <b>Open Source</b> blogging.
+
+## GitHub Pages
+GitHub Pages is a static site hosting service & uses Jekyll as the default generator to build websites from a GitHub repository.
+
+* [https://pages.github.com/][GitHubPages]
+
+## GitHub
+GitHub provides repository hosting for software development version control using Git.
+
+* [https://github.com/][GitHub]
+* [https://guides.github.com/][GitHubGuides]
+
+## Git 
+Git is a distributed version-control system for tracking changes in source code during software development.
+
+* [https://git-scm.com/downloads][Git]
+
+## Visual Studio Code
+Visual Studio Code is a source-code editor developed by Microsoft with support for Git and Markdown editing.
+
+* [https://code.visualstudio.com/download][VisualStudioCode]
+
+## Jekyll 
+Jekyll is an open source static site generator built as a Ruby Gem and integrated into GitHub Pages.
+
+* [https://jekyllrb.com/][Jekyll]
+* [https://jekyllrb.com/docs/installation/windows/][JekyllWindowsInstall]
+
+## Ruby 
+Ruby is an open source Programming Language.
+
+* [https://rubyinstaller.org/downloads/][RubyInstaller]
+
+[Jekyll]:https://jekyllrb.com/
+[JekyllWindowsInstall]:https://jekyllrb.com/docs/installation/windows/
+[GitHubPages]:https://pages.github.com/
+[GitHub]:https://github.com/
+[Git]:https://git-scm.com/downloads
+[GitHubGuides]:https://guides.github.com/
+[VisualStudioCode]:https://code.visualstudio.com/download
+[RubyInstaller]:https://rubyinstaller.org/downloads/
+[MinimalMistakes]:https://mmistakes.github.io/minimal-mistakes/
+[MinimalMistakesQuickStart]:https://mmistakes.github.io/minimal-mistakes/docs/quick-start-guide/
---
+++ rafts/assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "AD FS 2016 and InvalidNameIDPolicy using SAML Authentication to SailPoint IdentityNow",
+        "excerpt":"I recently had a seemingly simple task for a customer to setup a AD FS 2016 relying party trust for their SailPoint IdentityNow deployment. Sounds easy right? In this scenario AD FS 2016 was to be the Identity Provider (IdP) and IdentityNow the Service Provider (SP). Our end-goal of the...","categories": ["CONSULTING"],
+        "tags": ["ADFS","SAML","CLAIMS","SAILPOINT IDENTITYNOW","INVALIDSPNAMEQUALIFIER"],
+        "url": "http://localhost:4000/consulting/AD-FS-2016-and-InvalidNameIDPolicy-using-SAML-Authentication-to-SailPoint-IdentityNow/",
+        "teaser":null},{
+        "title": "Zero-Touch BitLocker with PowerShell",
+        "excerpt":"The majority of IT engineers and architects traverse various forms of security on a daily basis ranging from our complex alphanumeric corporate logon passwords to the increasingly common MFA prompts on our mobiles. You could say that we have become experts in navigating modern security measures required to stay protected....","categories": ["CONSULTING"],
+        "tags": ["BITLOCKER","WINDOWS","POWERSHELL","AUTOMATION"],
+        "url": "http://localhost:4000/consulting/Zero-Touch-BitLocker-with-Powershell/",
+        "teaser":null},{
+        "title": "0.09ms using Azure Proximity Placement Groups",
+        "excerpt":"Reducing network latency for critical apps running on Azure IaaS has become easier since Microsoft’s announcement of General Availability for Proximity Placement Groups (PPG) on Dec 2019. Today I’ll give you a quick intro to Proximity Placement Groups demonstrating how to deploy a test environment into your Azure Subscription using...","categories": ["CONSULTING"],
+        "tags": ["AZURE","AZURECLI","HOW-TO","PROXMITY PLACEMENT GROUP"],
+        "url": "http://localhost:4000/consulting/0.09ms-using-Azure-Proximity-Placement-Groups/",
+        "teaser":null},{
+        "title": "Your 2020 Study Guide to Azure DevOps Solutions (AZ-400)",
+        "excerpt":"With Microsoft announcing sweeping changes to a few Azure exams later in March 2020 the time is ripe to blog about an exam I’ve had my eye on for a while: Azure DevOps Solutions (AZ-400). This is an advanced exam that targets professionals familiar with Azure administration and Azure development....","categories": ["EXAMS"],
+        "tags": ["AZ-400","STUDY GUIDE"],
+        "url": "http://localhost:4000/exams/Your-2020-Study-Guide-to-Azure-DevOps-Solutions-(AZ-400)/",
+        "teaser":null},{
+        "title": "Quick Intro to Azure Backup Explorer",
+        "excerpt":"Here’s what you need to know about Azure Backup Explorer which has been released in public preview as of 5th Feb 2020. What is Azure Backup Explorer? It’s currently available for use with Azure Virtual Machines A single pane of glass for monitoring backups of your Azure Virtual Machines Removes...","categories": ["CONSULTING"],
+        "tags": ["AZURE","BACKUP EXPLORER","MONITORING"],
+        "url": "http://localhost:4000/consulting/Quick-Intro-to-Azure-Backup-Explorer/",
+        "teaser":null}]
